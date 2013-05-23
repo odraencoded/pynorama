998d8dd036647a5ee2ac2111b74998ee7608e418
Leonardo A. Pereira <odraencoded@gmail.com>
2013-05-23 20:47:14 -0300

    Welcome to v0.2.0!

--- pynorama.py
+++ pynorama.py
@@ -27,7 +27,7 @@ from loading import DirectoryLoader
 DND_URI_LIST, DND_IMAGE = range(2)
 
 class ImageViewer(Gtk.Application):
-	Version = "v0.1.3"
+	Version = "v0.2.0"
 	
 	def __init__(self):
 		Gtk.Application.__init__(self)
