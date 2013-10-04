7ae027be15120a6a2e75bdbe66e0860dd81eab3b
Leonardo A. Pereira <odraencoded@gmail.com>
2013-10-04 13:38:50 -0300

    Added an utility class for connecting signals
    It monitors a GProperty of a GObject that should be of
    a GObject type and automatically disconnects and
    reconnects signal handlers when that GProperty changes

--- code/pynorama/utility.py
+++ code/pynorama/utility.py
@@ -149,6 +149,51 @@ class Point(namedtuple("Point", ("x", "y"))):
         return self[0] ** 2 + self[1] ** 2
 
 
+class SignalHandlerConnector:
+    def __init__(self, source, source_property, **kwargs):
+        self.source = source
+        self.source_property = source_property
+        self.signal_handlers = kwargs
+        
+        self._notify_signal_id = source.connect(
+            "notify::" + source_property, self._changed_target_cb
+        )
+        
+        self.target = source.get_property(source_property)
+        
+        self._target_signal_ids = None
+        self.reconnect()
+    
+    
+    def disconnect(self):
+        if self.target and self._target_signal_ids:
+            for a_signal_id in self._target_signal_ids:
+                self.target.disconnect(a_signal_id)
+            self._target_signal_ids = None
+        
+    
+    def reconnect(self):
+        if self.target and not self._target_signal_ids:
+            self._target_signal_ids = [
+                self.target.connect(a_signal, a_handler)
+                for a_signal, a_handler in self.signal_handlers.items()
+            ]
+        
+        
+    def destroy(self):
+        self.disconnect()
+        self.source.disconnect(self._notify_signal_id)
+        del self.source, self.target, self.signal_handlers
+    
+    
+    def _changed_target_cb(self, *whatever):
+        new_target = self.source.get_property(self.source_property)
+        if self.target != new_target:
+            self.disconnect()
+            self.target = new_target
+            self.reconnect()
+
+
 Point.Zero = Point(0, 0)
 Point.Center = Point(.5, .5)
 Point.One = Point(1, 1)
