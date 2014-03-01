444abf3f90c9bb713ea2317c3b34161f80ec4e37
Leonardo A. Pereira <odraencoded@gmail.com>
2014-03-01 14:31:41 -0300

    Fixed local run script to create preferences in the right place every time

--- code/run.py
+++ code/run.py
@@ -4,11 +4,11 @@ from pynorama.application import ImageViewer
 import sys, os
 
 # Set preferences directory path to ../preferences/
-directory = os.path.dirname(__file__)
+directory = os.path.dirname(os.path.abspath(__file__))
 parent_directory = os.path.dirname(directory)
-preferences_directory = os.path.join(parent_directory, "..", "preferences")
-data_directory = os.path.join(parent_directory, "..", "resources")
-abs_parent_directory = os.path.abspath(directory)
+
+preferences_directory = os.path.join(parent_directory, "preferences")
+data_directory = os.path.join(parent_directory, "resources")
 
 ImageViewer.PreferencesDirectory = preferences_directory
 ImageViewer.DataDirectory = data_directory
